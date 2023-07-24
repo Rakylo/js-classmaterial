8c1618d7fee316c8dad33b337a362632ae1c5230
Martynas_Rakauskas <martynas@rakauskas.eu>
2023-07-24 20:47:42 +0300

    javascript1

---
+++ index.js
@@ -0,0 +1,29 @@
+// console.log(123);
+
+const name = "Martynas" ;
+let age = 27;
+const lastName = "Rakauskas" ;
+console.log(name);
+console.log(lastName);
+
+console.log(age);
+age = 30;
+console.log(age);
+const sum = 10+15;
+console.log(sum);
+
+const sub = 10-7;
+console.log(sub);
+
+const mult = 3 * 8;
+console.log(mult);
+
+const div = 6 / 2;
+console.log(div);
+
+const sum1 = "Labas" + "ate";
+console.log(sum1); 
+
+const sub3 = age + 7;
+console.log(sub3);
+console.log(age);
---
+++ uzduotis 1/index.js
@@ -0,0 +1,24 @@
+//   Uzduotis1
+// console.log("Hello World");
+// alert("I love Javascript!");
+// alert("Ready to learn more");
+// const number = prompt("Ivesk skaiciu nuo 1 iki 10")
+// console.log(number);
+
+//     Uzduotis2
+// const name = prompt("Iveskite savo varda");
+// const birthDay = "Geguzes 12";
+
+// const fullText = name + " yra gimes " + birthDay;
+// console.log(fullText);
+
+//    Uzduotis3
+const bazinisUzmokestis = prompt(' Iveskite bazini uzmokesti ');
+const workingPeople = 1761463;
+const budget = 8487300000;
+const precent = workingPeople * bazinisUzmokestis / budget * 100;
+document.write(precent);
+
+// su defer neveikia document.write
+
+
